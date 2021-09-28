03c09d7910b9a2c6aefbd38370a5fb6793434473
Kaytee07 <kofi.taylor07@gmail.com>
2021-09-28 22:04:21 +0000

    almost all buttons and modal are fully functional

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,14 @@ const $cover = document.querySelector(".cover");
 const $headerOptions = document.querySelector(".header__options");
 const $body = document.querySelector("body");
 const $logo = document.querySelector(".header__nav__logo")
-const $location = document.querySelector(".location");
-const $more = document.querySelector(".more");
-const $explore = document.querySelector(".explorer");
-const $tour = document.querySelector(".tours");
+const $location = document.querySelectorAll(".location");
+const $more = document.querySelectorAll(".more");
+const $explore = document.querySelectorAll(".explorer");
+const $tour = document.querySelectorAll(".tours");
 const $closeicon = document.querySelector(".close-icon");
 const $cardBtn = document.querySelectorAll(".card-btn");
 const $describtionModal = document.querySelector(".describtion__modal");
+const $overlay = document.querySelector(".overlay2");
 
 
 
@@ -34,20 +35,35 @@ $logo.addEventListener("click", ()=>{
     window.location.href = 'index.html'
 })
 
-$location.addEventListener("click", () => {
 
-  this.location.href = "locations.html";
-});
 
 // $more.addEventListener("click", () => {
 //   this.location.href = "locations.html";
 // });
-
+$more.forEach((more) => {
+  more.addEventListener("click", () => {
+    this.location.href = "locations.html";
+  });
+});
  
 
-// $explore.addEventListener("click", () => {
-//  this.location.href = "locations.html";
-// });
+$location.forEach((locate)=>{
+ locate.addEventListener("click", ()=>{
+   this.location.href = "locations.html";
+ })
+})
+
+$explore.forEach((explore) => {
+  explore.addEventListener("click", () => {
+    this.location.href = "locations.html";
+  }); 
+});
+
+$tour.forEach((tour) => {
+  tour.addEventListener("click", () => {
+    this.location.href = "locations.html";
+  });
+});
 
 // $tour.addEventListener("click", () => {
 //   this.location.href = "locations.html";
@@ -56,11 +72,16 @@ $location.addEventListener("click", () => {
 $cardBtn.forEach((card)=>{
   card.addEventListener("click", function(){
     $describtionModal.classList.add("show")
+     $body.classList.add("hidden");
+      $overlay.classList.add("show");
+     
   })
 })  
 
 $closeicon.addEventListener("click", ()=>{
   $describtionModal.classList.remove("show");
+  $body.classList.remove("hidden");
+   $overlay.classList.remove("show");
 })
 
 
