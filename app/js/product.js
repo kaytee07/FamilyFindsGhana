e5f8d919fd01879dfffbe646ca2affeeb1a86f19
kaytee07 <41648768+kaytee07@users.noreply.github.com>
2021-11-15 14:23:17 +0000

    directory corrections

--- app/js/product.js
+++ app/js/product.js
@@ -2,7 +2,7 @@ export const Product = [
   {
     name: "POOLFEST",
     Category: "Beaches and Resort",
-    img: "/img/food-img-1.jpg",
+    img: "img/food-img-1.jpg",
     about:
       "DJ Switch’s Mum is the most ungrateful person. Twene Jonas helped made good decisions and through that, the kid made it world wide. Now she’s spreading fake news about him to the politicians…. Trust No body herhh",
     location:
@@ -11,7 +11,7 @@ export const Product = [
   {
     name: "POOLFEST",
     Category: "Beaches and Resort",
-    img: "/img/food-img-1.jpg",
+    img: "img/food-img-1.jpg",
     about:
       "DJ Switch’s Mum is the most ungrateful person. Twene Jonas helped made good decisions and through that, the kid made it world wide. Now she’s spreading fake news about him to the politicians…. Trust No body herhh",
     location:
@@ -21,7 +21,7 @@ export const Product = [
     name: "BEACH BASH",
     Dates: "2nd January 2022",
     Category: "Play Ground and Parks",
-    img: "/img/food-img-2.jpg",
+    img: "img/food-img-2.jpg",
     about:
       "Ah After listening to the lady i have really regretted voting for NPP you can book plane for people to go to America to search for twene jonas eii ohhh dabi dabi",
     location:
@@ -32,7 +32,7 @@ export const Product = [
     name: "SANDBATH BASH",
     Dates: "1st June 2022",
     Category: "Hotels",
-    img: "/img/photo-1471914036897-d8255336ca8a.jpg",
+    img: "img/photo-1471914036897-d8255336ca8a.jpg",
     about:
       "ut the saydadon guy and Twene Jonas them dey fool or what? Twene Jonas is missen and you print shirt kot pic, ebi showbiz them dey do ",
     location:
@@ -42,7 +42,7 @@ export const Product = [
     name: "ICE CREAM PALACE",
     Location: "NORTH KANESHIE",
     Category: "Sweet Tooth",
-    img: "/img/ice-cream-image-2.jpg",
+    img: "img/ice-cream-image-2.jpg",
     about:
       "In the Era of Democracy , if you talk against Bad Leadership, You suddenly get Missing. Twene Jonas is missing just like that?",
     location:
@@ -54,7 +54,7 @@ export const Product = [
     name: "PIZZA PALACE ",
     Location: "EAST LEGON",
     Category: "Food and Restaurant",
-    img: "/img/pizza-1.jpg",
+    img: "img/pizza-1.jpg",
     about:
       "Twene Jonas is declared missing by his sister and best friend “say the Don” ,... hoping wanna leaders are not involved ",
     location:
